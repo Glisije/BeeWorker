e0dda7971dc96d7da5be4b26fe6fa31e44d3f9e9
glisije <prod@romas-arturovich.ru>
2023-06-07 15:45:03 +0300

    Move Activity and add Room

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.room.Room
+import com.glisije.clearsalary.Data.MyAppDatabase
+import com.glisije.clearsalary.databinding.ActivityMainBinding
+import com.glisije.clearsalary.ui.setDay.SetDayFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    public val database by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            MyAppDatabase::class.java,
+            "my_database"
+        ).build()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Change the method call here
+
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_profile, R.id.navigation_setDay, R.id.navigation_setSalary
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
